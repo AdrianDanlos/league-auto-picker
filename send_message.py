5efc0bf2b6457d83404241ae3cf569425591157a
Adrian Danlos <danlosadrian@gmail.com>
2025-07-25 19:33:26 +0700

    message fixed

--- send_message.py
+++ send_message.py
@@ -1,11 +1,15 @@
 # flake8: noqa: E501
 import requests
 
+
 def send_champ_select_message(session, base_url, auth, message):
     # Try to get the chatId from session
     chat_id = None
-    if "chatDetails" in session and "chatId" in session["chatDetails"]:
-        chat_id = session["chatDetails"]["chatId"]
+    if "chatDetails" in session:
+        # Try both possible keys
+        chat_id = session["chatDetails"].get("chatId") or session["chatDetails"].get(
+            "multiUserChatId"
+        )
     elif "chatId" in session:
         chat_id = session["chatId"]
     if chat_id:
